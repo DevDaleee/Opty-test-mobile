ee3343f7f5e9a71b0502544150e533ae6eb927ba
DevDaleee <marcosdalessandroclima@ufpi.edu.br>
2024-12-08 19:02:56 -0300

    chore(API): Updating api address to live API

--- lib/services/constants.dart
+++ lib/services/constants.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 
 class ApiConstants {
-  static var basePath = 'http://localhost:3333';
+  static var basePath = 'https://api-finance-pv8i.onrender.com';
   static Dio dio = Dio();
   // dio.options.connectTimeout = const Duration(seconds: 10);
   // dio.options.receiveTimeout = const Duration(seconds: 10);
